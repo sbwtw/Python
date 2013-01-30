51af8e1d883a078221868f89f8f9664d6b4b3575
sbwtw <sbwtws@gmail.com>
2013-01-31 03:58:34 +0800

    首版本,可登陆,拉取贴吧列表,不可签到.

--- 百度贴吧签到神器.py
+++ 百度贴吧签到神器.py
@@ -8,8 +8,8 @@ import urllib2
 import cookielib
 
 # 预定义账号密码
-usr='o丨Reborn'
-pwd='13579dsd'
+usr=''
+pwd=''
 # headers
 headers={
 	'User-Agent':'Mozilla/5.0 (iPhone; U; CPU iPhone OS 4_0 like Mac OS X; en-us) AppleWebKit/532.9 (KHTML, like Gecko) Version/4.0.5 Mobile/8A293 Safari/6531.22.7'
